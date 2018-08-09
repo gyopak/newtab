aba043498adcc0e4a321666b9b8c9fb3ecc81507
gy0p4k <gyorgypakozdi@gmail.com>
2018-08-09 15:42:20 +0800

    :sparkles: working search with redirect

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const search = document.querySelector('.searchbar')
 const preSearch = '$ ';
+const searchUrl = 'https://www.google.com.ph/search?q='
 
-const typeHandler = (event) => {
-    console.log(event);
-    search.value = search.value.includes(preSearch) ? search.value : `${preSearch}`;
+const typeHandler = () => search.value = search.value.includes(preSearch) ? search.value : `${preSearch}`;
+
+const searchHandler = () => {
+    const query = search.value.split(preSearch)[1].replace(' ', '+');
+    window.location.replace(`${searchUrl + query}`);
 }
 
 (() => {
   search.value = preSearch;
   search.focus();
   search.addEventListener('input', typeHandler);
+  search.addEventListener('change', searchHandler);
 })()
